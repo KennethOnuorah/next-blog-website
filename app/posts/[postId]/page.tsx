b96689dfd3537828b3a57c9f897f7f6c308d810c
kenneth4832@gmail.com <kenneth4832@gmail.com>
2024-02-15 11:55:26 -0600

    Changed @lib files to .ts, added read time approximation

--- app/posts/[postId]/page.tsx
+++ app/posts/[postId]/page.tsx
@@ -3,9 +3,11 @@ import { notFound } from "next/navigation"
 import ThemedMarkdown from "./components/ThemedMarkdown"
 
 import getFormattedDate from "@/lib/getFormattedDate"
+import getApproximateReadTime from "@/lib/getApproximateReadTime"
 import { getPostByName, getAllPosts } from "@/lib/posts"
 
 import { FaHome as Home } from "react-icons/fa"
+import { FaRegClock as Time } from 'react-icons/fa6'
 
 export const revalidate = 0
 
@@ -49,16 +51,21 @@ export default async function PostPage({params: {postId}}: Props) {
     <main className="px-6 prose prose-xl mx-auto dark:prose-invert">
       <h2 className="text-4xl text-center mt-12 mb-4 font-bold sm:text-left">{meta.title}</h2>
       <p className="text-2xl text-center italic text-gray-500 sm:text-left">{publishedDate}</p>
-      <div className="flex justify-center gap-2 mb-6 sm:justify-start">
-        {meta.tags.map((tag, index) => 
-          <Link  
-            key={index}
-            href={`/tags/${tag}`} 
-            className="no-underline text-xs px-2 py-1 bg-gray-200 rounded-full hover:underline dark:bg-gray-600 dark:text-white"
-          >
-            {tag}
-          </Link>
-        )}
+      <div className="flex justify-center flex-col items-center gap-4 sm:justify-start sm:flex-row">
+        <div className="flex gap-2 flex-wrap justify-center">
+          {meta.tags.map((tag, index) => 
+            <Link  
+              key={index}
+              href={`/tags/${tag}`} 
+              className="no-underline text-xs px-2 py-1 bg-gray-200 rounded-full hover:underline dark:bg-gray-600 dark:text-white"
+            >
+              {tag}
+            </Link>
+          )}
+        </div>
+        <div className="flex items-center gap-2 text-sm text-gray-500">
+          <Time/> {getApproximateReadTime(content)}
+        </div>
       </div>
       <ThemedMarkdown>{content}</ThemedMarkdown>
       <p className="mb-10">
---
+++ lib/getApproximateReadTime.ts
@@ -0,0 +1,16 @@
+const AVERAGE_WPM = 238
+
+export default function getApproximateReadTime(content: string): string {
+  const wordLength = content.split(' ').length
+  const periodPauseTime = content.match(/\. /g || [])?.length! * 0.0083
+  const commaPauseTime = content.match(/\, /g || [])?.length! * 0.006
+  
+  const totalReadTime = (wordLength / AVERAGE_WPM) + periodPauseTime + commaPauseTime
+
+  if(totalReadTime >= 1){
+    return `${totalReadTime.toFixed(1)} min read`
+  }else{
+    const seconds = parseFloat(totalReadTime.toFixed(1)) * 60
+    return `${(seconds)} sec read`
+  }
+}
